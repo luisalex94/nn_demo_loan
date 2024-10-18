94523e4eb1d66a7297b25b967bf031174718cd68
AlejandroJR <luisalex944@gmail.com>
2024-10-18 11:49:53 -0600

    first testing with KonfioTesting.py

---
+++ KonfioTesting/lambda_function.py
@@ -0,0 +1,14 @@
+print('Counter 00001')
+print('INIT - KonfioTesting 000')
+
+import json
+
+print('INIT - KonfioTesting 001')
+
+def lambda_handler(event, context):
+    # TODO implement
+    print('INIT - KonfioTesting 002')
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
