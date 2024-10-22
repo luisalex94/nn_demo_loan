7cabe265e850a673e45e255c71775cdbc29c3bcc
AlejandroJR <luisalex944@gmail.com>
2024-10-22 13:18:55 -0600

    code added to app.py microservice, pending resolve model.eval

--- model_microservice/app.py
+++ model_microservice/app.py
@@ -1,6 +1,6 @@
 import joblib
-#import torch
-#import torch.nn as nn
+import torch
+import torch.nn as nn
 import pandas as pd
 import logging
 from flask import Flask, request, jsonify
@@ -8,6 +8,34 @@ from flask import Flask, request, jsonify
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 app = Flask(__name__)
+
+
+# Define the neural network
+class LoanApprovalNN(nn.Module):
+    def __init__(self):
+        super(LoanApprovalNN, self).__init__()              # Call the constructor of the parent class
+        self.fc1 = nn.Linear(26, 64)   # Define the first fully connected layer
+        self.fc2 = nn.Linear(64, 64)                        # Define the second fully connected layer
+        self.fc3 = nn.Linear(64, 64)                        # Define the third fully connected layer
+        self.fc4 = nn.Linear(64, 32)                        # Define the fourth fully connected layer
+        self.output = nn.Linear(32, 1)                      # Define the output layer
+    
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))             # Apply the ReLU activation function to the first fully connected layer
+        x = torch.relu(self.fc2(x))             # Apply the ReLU activation function to the second fully connected layer
+        x = torch.relu(self.fc3(x))             # Apply the ReLU activation function to the third fully connected layer
+        x = torch.relu(self.fc4(x))             # Apply the ReLU activation function to the fourth fully connected layer  
+        x = torch.sigmoid(self.output(x))       # Apply the sigmoid activation function to the output layer
+        return x
+
+# Instantiate the model
+model = LoanApprovalNN()
+
+# Load the model
+model = torch.load('model.pth')
+
+# Set the model to evaluation mode
+# model.eval()
 
 
 @app.route('/saludo', methods=['POST'])
